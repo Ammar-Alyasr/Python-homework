2cd2079124079ebdb144c5dd0fa37af883c64124
Ammar-Alyasr <you@example.com>
2017-05-11 21:19:59 +0300

    adding

--- yy.py
+++ yy.py
@@ -1,8 +1,10 @@
+import tkinter
 from tkinter import *
 from tkinter import Message
 import time, locale
 import sqlite3
 from tkinter.ttk import Panedwindow
+
 
 vt = sqlite3.connect("dukkan.db")
 im = vt.cursor()
@@ -10,7 +12,13 @@ im.execute("CREATE TABLE IF NOT EXISTS kiralayicilar(adSoyad,Tc,saat)")
 root = Tk()
 root.minsize(500, 400)
 root.maxsize(500, 400)
-root.title("Bisekletler kıralam için ...")
+root.title("Biseklet Kiralama ...")
+
+# logo = PhotoImage(file = 'resim.jpg')
+# background_label = Label(root, image=logo)
+# background_label.image = logo
+# background_label.place(x=2, y=2, relwidth=1, relheight=1)
+
 
 
 def kiralayici():
@@ -33,19 +41,22 @@ def kiralayici():
     txttc.grid(row=1, column=1)
 
     b2 = Button(kiralayiciSayfasi, text="AL", width=10, command=al)
-    b2.grid(row=5, column=1)
-
+    b2.grid(row=5,column=1)
     b3 = Button(kiralayiciSayfasi, text="Iade Et", width=10, command=iade)
-    b3.grid(row=5, column=2)
+    b3.grid(row=5,column=3)
 
 
 def al():
+
+
+
     im.execute("INSERT INTO kiralayicilar (adsoyad,Tc,saat) VALUES(?,?,?)",
-               (txtisim.get(), txttc.get(), time.strftime("%H:%M:%S")))
+            (txtisim.get(), txttc.get(), time.strftime("%H:%M:%S")))
     vt.commit()
     im.execute("SELECT * FROM kiralayicilar")
     veriler = im.fetchall()
     print(veriler)
+
 
 
 def iade():
@@ -54,7 +65,75 @@ def iade():
     im.execute("SELECT * FROM kiralayicilar WHERE Tc=(?)", [txttc.get()])
     veriler = im.fetchall()
     print(veriler)
-    print(veriler[0][2])
+    print(veriler[0][2][1])
+
+    a = veriler[0][2][0]
+    b = veriler[0][2][1]
+    c = a + b #Girissaat
+    c=int(c)
+    print(c)
+
+    f = veriler[0][2][3]
+    t = veriler[0][2][4]
+    d = f + t  # giris dakika kısmı
+    d = int(d)
+    print(d)
+
+    cikisZamani = time.strftime("%H %M") #cikis SAAT
+    saat1 = cikisZamani[0]
+    saat2 = cikisZamani[1]
+    saat = saat1 + saat2
+    saat = int(saat)#cikis SAAT
+    print(saat)
+
+    dakika1 = cikisZamani[3]
+    dakika2 = cikisZamani[4]
+    dakika = dakika1 + dakika2
+    dakika = int(dakika)
+    print(dakika)
+
+    saat_farkı = 0
+    dakika_farkı = 0
+    toplam_saat = 0
+    if saat > c:
+        if dakika > d:
+            dakika_farkı = dakika - d
+            saat_farkı = saat - c
+            toplam_dakika = (saat_farkı * 60) + dakika_farkı
+            toplam_saat = toplam_dakika / 60
+        elif dakika < d:
+            dakika_farkı = (dakika + 60) - d
+            saat = saat - 1
+            saat_farkı = saat - c
+            toplam_dakika = (saat_farkı * 60) + dakika_farkı
+            toplam_saat = toplam_dakika / 60
+
+
+    elif saat < c:
+        if dakika > d:
+            dakika_farkı = dakika - d
+            saat_farkı = (24 - c) + saat
+            toplam_dakika = (saat_farkı * 60) + dakika_farkı
+            toplam_saat = toplam_dakika / 60
+        elif dakika < d:
+            dakika_farkı = (dakika + 60) - d
+            saat = saat - 1
+            saat_farkı = (24 - c) + saat
+            toplam_dakika = (saat_farkı * 60) + dakika_farkı
+            toplam_saat = toplam_dakika / 60
+
+            print(toplam_dakika)
+    print("saat farkı:{0}, dakika farkı: {1}, toplam geçen süre saat cinsinden:{2}".format(saat_farkı, dakika_farkı,
+                                                                                           toplam_saat))
+
+    toplam_saat = float(toplam_saat)
+
+    sonsonuc = toplam_saat * 4.0  # bir saatlik ucreti 4 TL dir
+    # sonsonuc=float(sonsonuc)
+    sonsonuc = round(sonsonuc, 2)
+    print(sonsonuc)
+
+
 
     kiralayiciSayfasi3 = Tk()
     lbliade = Label(kiralayiciSayfasi3, text="isim").pack(padx=10, pady=5, side=TOP)
@@ -70,6 +149,9 @@ def iade():
     b5.pack(padx=20, pady=50, side=LEFT)
     b6 = Button(kiralayiciSayfasi3, text="Iade Et", width=10)
     b6.pack(padx=20, pady=50, side=RIGHT)
+
+    im.execute("DELETE FROM kiralayicilar where Tc=(?)", [txttc.get()])
+    vt.commit()
 
 
 def yonetici():
